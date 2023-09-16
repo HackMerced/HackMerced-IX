541ad568387d11163be1fc220f0babcb10a39914
mustachemo <mhasan1080@gmail.com>
2023-09-16 10:54:25 -0700

    progress

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,13 +3,9 @@ import React, { useState } from 'react';
 function NavBar() {
   const [isNavBarActive, setIsNavBarActive] = useState(false);
 
-  const toggleNavBar = () => {
-    setIsNavBarActive(!isNavBarActive);
-  };
-
     return (
-      <div className="NavBar">
-      <a href="#" className={`navbar-toggle ${isNavBarActive ? 'active' : ''}`} onClick={toggleNavBar}>
+      <nav className={`NavBar ${isNavBarActive ? 'active' : ''}`}>
+        <a href="#" className="navbar-toggle" onClick={() => setIsNavBarActive(!isNavBarActive)}>
             <span class="bar"></span>
             <span class="bar"></span>
             <span class="bar"></span>
@@ -23,7 +19,7 @@ function NavBar() {
           <li><a href="https://docs.google.com/forms/d/1_A3jLqUO3d5hsv9MtMF358K0C2nntOpq_WYin2H1aKw/viewform"><button className="registerButton" id="register-button">register</button></a></li>
         </ul>
             
-      </div>
+      </nav>
     );
   }
   
